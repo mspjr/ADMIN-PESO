5418880994b190b0a0ed835bc7a9a68874cd0657
pisonmariojr <pisonmariojr@nddu.edu.ph>
2025-11-16 22:33:24 +0800

    file re-structure
    separated javascript file from html to js for dashboard.html

---
+++ dashboard.js
@@ -0,0 +1,17 @@
+   function toggleProfileMenu() {
+      const profileMenu = document.getElementById("profile-menu");
+      profileMenu.classList.toggle("show");
+    }
+
+    document.querySelectorAll('.toggle-menu').forEach(btn => {
+      btn.addEventListener('click', e => {
+        e.preventDefault();
+        const submenu = btn.nextElementSibling;
+
+        document.querySelectorAll('.submenu').forEach(list => {
+          if (list !== submenu) list.classList.remove('show');
+        });
+
+        submenu.classList.toggle('show');
+      });
+    });
